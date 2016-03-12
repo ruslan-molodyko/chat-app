233b1a3628d626e7574d22d2fd0aa8aa2d8964ee
ruslan-molodyko <ruslan.molodyko@gmail.com>
2016-03-12 13:40:22 +0200

    Changed port

--- app.js
+++ app.js
@@ -44,17 +44,17 @@ server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
   console.log('Application worker ${process.pid} started...');
 });
 
-///**
-// * Created by rmolodyko on 25.01.2016.
-// */
-//var io = require('socket.io').listen(8080);
-//
-//// Wait connections
-//io.sockets.on('connection', function (socket) {
-//
-//    // If message was sent then send response broadcast and directly to user
-//    socket.on('message', function (msg) {
-//        socket.json.send(msg);
-//        socket.broadcast.json.send(msg)
-//    });
-//});
+/**
+ * Created by rmolodyko on 25.01.2016.
+ */
+var io = require('socket.io').listen(8000);
+
+// Wait connections
+io.sockets.on('connection', function (socket) {
+
+    // If message was sent then send response broadcast and directly to user
+    socket.on('message', function (msg) {
+        socket.json.send(msg);
+        socket.broadcast.json.send(msg)
+    });
+});
